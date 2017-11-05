de396329ebb18db66b763c631539c3eb58b10156
Brad Bohen <bbohen@gmail.com>
2017-11-05 00:14:35 -0400

    Added koa-cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const koaBody = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const mount = require('koa-mount');
 const serve = require('koa-static');
 const path = require('path');
@@ -9,14 +10,11 @@ const router = require('./router');
 
 const app = new Koa();
 
-// body parser
+app.use(cors());
 app.use(koaBody());
-// custom graphiql with auth token integration
 app.use(mount('/graphiql', serve(path.resolve(__dirname, '..', 'graphiql'))));
-// auth middleware that adds the auth token to the request context
 app.use(auth);
-// set up the router
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(8080, () => console.log('listening on 8080')); // eslint-disable-line
+app.listen(8080, () => console.log("listening on 8080")); // eslint-disable-line
